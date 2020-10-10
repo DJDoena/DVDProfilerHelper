d1354a2cba206953ebea792eadc1a288506af527
Karsten Schramm <SchramKa@basfad.basf.net>
2020-10-10 19:37:52 +0200

    2.2.3.5

--- DVDProfilerHelper/Properties/AssemblyInfo.cs
+++ DVDProfilerHelper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doena Soft.")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © Doena Soft. 2012 - 2018")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2012 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.2.3.4")]
-[assembly: AssemblyFileVersion("2.2.3.4")]
+[assembly: AssemblyVersion("2.2.3.5")]
+[assembly: AssemblyFileVersion("2.2.3.5")]
 
 [assembly: AssemblyDelaySign(false)]
