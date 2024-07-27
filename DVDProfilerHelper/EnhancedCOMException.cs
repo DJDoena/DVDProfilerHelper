6b2de65121b54eeadb45c911164f084a86b5e286
DJ Doena <djdoena@outlook.com>
2024-07-27 21:42:13 +0200

    DVDProfiler.Helper 3.0.9

--- DVDProfilerHelper/EnhancedCOMException.cs
+++ DVDProfilerHelper/EnhancedCOMException.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
 
 namespace DoenaSoft.DVDProfiler.DVDProfilerHelper
 {
@@ -11,40 +10,67 @@ namespace DoenaSoft.DVDProfiler.DVDProfilerHelper
 
         private readonly COMException _originalException;
 
-        public EnhancedCOMException(string lastApiError, COMException originalException) : base(lastApiError, originalException)
+        public EnhancedCOMException(string lastApiError, COMException originalException)
+            : base(lastApiError, originalException)
         {
-            LastApiError = lastApiError;
+            this.LastApiError = lastApiError;
             _originalException = originalException;
         }
 
-        public override IDictionary Data => _originalException?.Data ?? base.Data;
+        public override IDictionary Data
+            => _originalException?.Data ?? base.Data;
 
-        public override int ErrorCode => _originalException?.ErrorCode ?? base.ErrorCode;
+        public override int ErrorCode
+            => _originalException?.ErrorCode ?? base.ErrorCode;
 
         public override string HelpLink
         {
             get => _originalException?.HelpLink ?? base.HelpLink;
-            set { if (_originalException != null) { _originalException.HelpLink = value; } else { base.HelpLink = value; } }
+            set
+            {
+                if (_originalException != null)
+                {
+                    _originalException.HelpLink = value;
+                }
+                else
+                {
+                    base.HelpLink = value;
+                }
+            }
         }
 
-        public override string Message => _originalException?.Message ?? base.Message;
+        public override string Message
+            => _originalException?.Message ?? base.Message;
 
-        public override Exception GetBaseException() => _originalException?.GetBaseException() ?? base.GetBaseException();
-
-        public override void GetObjectData(SerializationInfo info, StreamingContext context) { if (_originalException != null) { _originalException.GetObjectData(info, context); } else { base.GetObjectData(info, context); } }
+        public override Exception GetBaseException()
+            => _originalException?.GetBaseException() ?? base.GetBaseException();
 
         public override string Source
         {
             get => _originalException?.Source ?? base.Source;
-            set { if (_originalException != null) { _originalException.Source = value; } else { base.Source = value; } }
+            set
+            {
+                if (_originalException != null)
+                {
+                    _originalException.Source = value;
+                }
+                else
+                {
+                    base.Source = value;
+                }
+            }
         }
 
-        public override string StackTrace => _originalException?.StackTrace ?? base.StackTrace;
+        public override string StackTrace
+            => _originalException?.StackTrace ?? base.StackTrace;
 
-        public override bool Equals(object obj) => _originalException?.Equals(obj) ?? base.Equals(obj);
+        public override bool Equals(object obj)
+            => _originalException?.Equals(obj) ?? base.Equals(obj);
 
-        public override int GetHashCode() => _originalException?.GetHashCode() ?? base.GetHashCode();
+        public override int GetHashCode()
+            => _originalException?.GetHashCode() ?? base.GetHashCode();
 
-        public override string ToString() => _originalException?.ToString() ?? base.ToString();
+        public override string ToString()
+            => _originalException?.ToString() ?? base.ToString();
     }
 }
