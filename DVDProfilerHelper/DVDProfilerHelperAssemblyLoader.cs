901b6b3f1cb71a151f53c148702f864ccfad1438
DJ Doena <djdoena@outlook.com>
2025-02-17 19:28:57 +0100

    3.0.11

---
+++ DVDProfilerHelper/DVDProfilerHelperAssemblyLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace DoenaSoft.DVDProfiler.DVDProfilerHelper
+{
+    public static class DVDProfilerHelperAssemblyLoader
+    {
+        public static void Load()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+        }
+
+        private static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            const string ExtensionAssemblyFileName = "System.Resources.Extensions";
+
+            if (args?.Name.StartsWith($"{ExtensionAssemblyFileName},") == true)
+            {
+                var thisAssemblyFile = new FileInfo(typeof(DVDProfilerHelperAssemblyLoader).Assembly.Location);
+
+                var extensionsAssemblyFile = new FileInfo(Path.Combine(thisAssemblyFile.DirectoryName, $"{ExtensionAssemblyFileName}.dll"));
+
+                if (File.Exists(extensionsAssemblyFile.FullName))
+                {
+                    var extensionsAssembly = Assembly.LoadFrom(extensionsAssemblyFile.FullName);
+
+                    return extensionsAssembly;
+                }
+            }
+
+            return null;
+        }
+    }
+}
